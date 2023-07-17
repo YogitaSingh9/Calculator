68cb860d9339a767e65a42ad169a33dec2b6cf80
YogitaSingh9 <115174566+YogitaSingh9@users.noreply.github.com>
2023-07-17 18:27:38 +0530

    Update First.js

--- Calculator/First.js
+++ Calculator/First.js
@@ -3,7 +3,7 @@ let buttons= document.querySelectorAll('button');
 let string="";
 let arr = Array.from(buttons);
 arr.forEach (button=> {
-    button.addEventListener('click',(e)=>{
+ button.addEventListener('click',(e)=>{
         if(e.target.innerHTML=='='){
             string=eval(string);
             input.value=string;
@@ -12,8 +12,14 @@ arr.forEach (button=> {
             string="";
             input.value=string;
         }
-        else{string+=e.target.innerHTML;
-        input.value=string;
+        else if(e.target.innerHTML=='DEL'){
+            string="";
+            string = string.substring(0, string.length-1);
+            input.value=string;
         }
-
+        else{
+            string+=e.target.innerHTML;
+            input.value=string;
+        }
     })
+})
